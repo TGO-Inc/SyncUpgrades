8a17e764ff3450d61510fef95866dedc7520ea82
TheGuy920 <53882381+TheGuy920@users.noreply.github.com>
2025-06-04 21:23:26 -0400

    Update CompilerServices.cs

--- Core/Internal/CompilerServices.cs
+++ Core/Internal/CompilerServices.cs
@@ -1,12 +1,18 @@
+// ReSharper disable once CheckNamespace
 namespace System.Runtime.CompilerServices;
 
+// ReSharper disable once UnusedType.Global
 internal static class IsExternalInit;
 
+// ReSharper disable once RedundantAttributeUsageProperty
 [AttributeUsage(AttributeTargets.Constructor | AttributeTargets.Property, Inherited = false)]
+// ReSharper disable once UnusedType.Global
 internal sealed class RequiredMemberAttribute : Attribute;
 
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, Inherited = false)]
+// ReSharper disable once UnusedType.Global
 internal sealed class CompilerFeatureRequiredAttribute : Attribute
 {
+    // ReSharper disable once UnusedParameter.Local
     internal CompilerFeatureRequiredAttribute(string featureName){ }
 }
